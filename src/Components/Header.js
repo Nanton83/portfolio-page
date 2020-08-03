aaa1bee74a0491c622d7585c7b93abdd917d5dc7
nanton83 <nicholas.romans@gmail.com>
2020-08-03 13:07:52 -0400

    background

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,7 +9,7 @@ class Header extends Component {
       var description= this.props.data.description;
       var city= this.props.data.address.city;
       var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+        return <li key={network.name}><a href={network.url} target="_blank"><i className={network.className}></i></a></li>
       })
     }
 
@@ -27,7 +27,7 @@ class Header extends Component {
 	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
             
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
+            
          </ul>
 
       </nav>
@@ -35,7 +35,7 @@ class Header extends Component {
       <div className="row banner">
          <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
+            <h3><span>I'm a {city} based {occupation}. {description}.</span></h3>
             <hr />
             <ul className="social">
                {networks}
